8e001ea6ed7fae563491779bcff8ad1d37b90eb6
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-10-15 13:11:30 -0400

    Add while loop

--- piggy_bank.rb
+++ piggy_bank.rb
@@ -8,3 +8,11 @@ end
 total = piggy_bank.inject(:+)
 
 puts "I have $#{total} in my piggy_bank!"
+
+while total > 10
+  total -= 1
+  puts "I spent $1 today. I have $#{total} left!"
+if total == 10
+    puts "I spent $5 today and have $#{total} left. I am going to stop spending money!"
+  end
+end
